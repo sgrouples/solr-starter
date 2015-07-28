b7a4298818f50f66b3e8ea352b7049ae2dd642a3
Marek Żebrowski <marek.zebrowski@gmail.com>
2015-07-28 12:11:26 +0200

    working version

--- src/main/java/me/sgrouples/SolrStarter.java
+++ src/main/java/me/sgrouples/SolrStarter.java
@@ -1,6 +1,13 @@
 package me.sgrouples;
 
+import org.apache.solr.client.solrj.embedded.JettyConfig;
 import org.apache.solr.client.solrj.embedded.JettySolrRunner;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Properties;
+
 
 class SolrStarter {
     public static void main(String[] args) {
@@ -11,8 +18,18 @@ class SolrStarter {
         if (args.length > 1) port = Integer.parseInt(args[1]);
         if (args.length > 2) context = args[2];
         try {
-            JettySolrRunner ex = new JettySolrRunner(path, context, port);
-            ex.start();
+            ServletHolder sh = new ServletHolder(org.restlet.ext.servlet.ServerServlet.class);
+            sh.setInitParameter("org.restlet.application", "org.apache.solr.rest.SolrSchemaRestApi");
+            Map<ServletHolder, String> solrServlets = Collections.singletonMap(sh, "/schema/*");
+            Properties nodeProps = new Properties();
+            JettyConfig jettyConfig = JettyConfig.builder()
+                    .setContext(context)
+                    .setPort(port)
+                    .withServlets(solrServlets)
+                    .build();
+            JettySolrRunner jettyRunner = new JettySolrRunner(path, nodeProps, jettyConfig);
+
+            jettyRunner.start();
         } catch (Exception var2) {
             var2.printStackTrace();
         }
