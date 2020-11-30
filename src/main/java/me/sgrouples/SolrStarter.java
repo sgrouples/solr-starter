eb812615877e92a15795806c2270518f844af56a
dbobowski <dbobowski@o2.pl>
2020-11-30 09:12:25 +0100

    Solr starter 2.0.0 (Solr 8.6.2)

--- src/main/java/me/sgrouples/SolrStarter.java
+++ src/main/java/me/sgrouples/SolrStarter.java
@@ -2,10 +2,7 @@ package me.sgrouples;
 
 import org.apache.solr.client.solrj.embedded.JettyConfig;
 import org.apache.solr.client.solrj.embedded.JettySolrRunner;
-import org.eclipse.jetty.servlet.ServletHolder;
 
-import java.util.Collections;
-import java.util.Map;
 import java.util.Properties;
 
 
@@ -18,14 +15,10 @@ class SolrStarter {
         if (args.length > 1) port = Integer.parseInt(args[1]);
         if (args.length > 2) context = args[2];
         try {
-            ServletHolder sh = new ServletHolder(org.restlet.ext.servlet.ServerServlet.class);
-            sh.setInitParameter("org.restlet.application", "org.apache.solr.rest.SolrSchemaRestApi");
-            Map<ServletHolder, String> solrServlets = Collections.singletonMap(sh, "/schema/*");
             Properties nodeProps = new Properties();
             JettyConfig jettyConfig = JettyConfig.builder()
                     .setContext(context)
                     .setPort(port)
-                    .withServlets(solrServlets)
                     .build();
             JettySolrRunner jettyRunner = new JettySolrRunner(path, nodeProps, jettyConfig);
 
